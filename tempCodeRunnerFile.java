b022fac5a6c7c0dc647d3fe50bf35bc7aa4a567d
Akash120806 <akashind5399@gmail.com>
2026-08-29 14:51:16 +0530

    java

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class RegularCleaning
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        int c=0;
+        if (N % 10 == 0){System.out.println(0);}
+        else{
+            c=10-(N%10);
+            System.out.println(c);
+        }
+ 
+	}
+}
